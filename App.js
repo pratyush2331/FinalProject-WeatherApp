a31084431771aa5ec31604616cc90817ff202b07
Pratyush Raj <72189926+pratyush2331@users.noreply.github.com>
2022-05-18 11:26:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,80 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import Weather from './components/Weather';
+import SearchBar from './components/SearchBar';
+
+const API_KEY = "00d2ad7cfd5d83ebde0972552006696e";
+
+
+export default function App() {
+
+    const [weatherData, setWeatherData] = useState(null);
+    const [loaded, setLoaded] = useState(true);
+
+    async function fetchWeatherData(cityName) {
+        setLoaded(false);
+        const API = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${API_KEY}`
+        try {
+            const response = await fetch(API);
+            if(response.status == 200) {
+                const data = await response.json();
+                setWeatherData(data);
+            } else {
+                setWeatherData(null);
+            }
+            setLoaded(true);
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchWeatherData('Chennai');
+    }, [])
+    
+    
+    if(!loaded) {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator color='gray'  size={36} />
+            </View>
+
+        )
+    }
+
+    else if(weatherData === null) {
+        return (
+            <View style={styles.container}>
+                <SearchBar fetchWeatherData={fetchWeatherData}/>
+                <Text style={styles.primaryText}>City Not Found! Try Different City</Text>
+            </View>
+        )
+    }
+
+    return (
+        <View style={styles.container}>
+            
+            <Weather weatherData={weatherData} fetchWeatherData={fetchWeatherData}  />
+            
+            <View style={{ alignContent: 'center'}}>
+                <Text>© designed by Pratyush Raj</Text>
+            </View>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  primaryText: {
+      margin: 20,
+      fontSize: 28
+  }
+});
